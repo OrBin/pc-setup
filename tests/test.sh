0dae771e6847de04adaa8f51484bead2248ad6f4
Or Bin <orbin50@gmail.com>
2021-12-22 10:19:08 +0200

    Added zoom

--- tests/test.sh
+++ tests/test.sh
@@ -6,6 +6,6 @@ TESTS_DIR=$(dirname "${BASH_SOURCE}")
 source "${TESTS_DIR}/compute_env.sh"
 
 multipass delete -p "${INSTANCE_NAME}" || true
-multipass launch --name "${INSTANCE_NAME}" "${UBUNTU_VERSION}"
+multipass launch --name "${INSTANCE_NAME}" --disk 10G "${UBUNTU_VERSION}"
 multipass mount "${REPO_ROOT}" "${INSTANCE_NAME}:/${REPO_NAME}"
 "${TESTS_DIR}/exec_setup.sh"
