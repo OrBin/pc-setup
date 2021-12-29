ce8e2f9f83d9f8ffe43e05a6190e54f091f9c2a0
Or Bin <orbin50@gmail.com>
2021-12-29 20:21:35 +0200

    Making multipass instance use all CPUs (now on s-8vcpu-16gb)

--- tests/test.sh
+++ tests/test.sh
@@ -5,7 +5,13 @@ TESTS_DIR=$(dirname "${BASH_SOURCE}")
 
 source "${TESTS_DIR}/compute_env.sh"
 
+cpu_flags=""
+
+if [ "$(systemd-detect-virt)" != "none" ]; then
+  cpu_flags="--cpus $(nproc)"
+fi
+
 multipass delete -p "${INSTANCE_NAME}" || true
-multipass launch --name "${INSTANCE_NAME}" --disk 10G "${UBUNTU_VERSION}"
+multipass launch --name "${INSTANCE_NAME}" --disk 10G "${cpu_flags[@]}" "${UBUNTU_VERSION}"
 multipass mount "${REPO_ROOT}" "${INSTANCE_NAME}:/${REPO_NAME}"
 "${TESTS_DIR}/exec_setup.sh"
