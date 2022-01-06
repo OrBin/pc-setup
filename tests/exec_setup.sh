3c759b8a318ba551aa31fb31a17ddc1a92035ce7
Or Bin <orbin50@gmail.com>
2022-01-06 23:58:50 +0200

    Added KDE (and GH CLI) configuration

--- tests/exec_setup.sh
+++ tests/exec_setup.sh
@@ -5,4 +5,4 @@ TESTS_DIR=$(dirname "${BASH_SOURCE}")
 
 source "${TESTS_DIR}/compute_env.sh"
 
-multipass exec "${INSTANCE_NAME}" -- bash -c "ANSIBLE_BECOME_ASK_PASS=0 ANSIBLE_DEBUG=1 /${REPO_NAME}/setup.sh --extra-vars \"@/${REPO_NAME}/tests/test_vars.yml\""
+multipass exec "${INSTANCE_NAME}" -- bash -c "ANSIBLE_BECOME_ASK_PASS=0 /${REPO_NAME}/setup.sh --extra-vars \"@/${REPO_NAME}/tests/test_vars.yml\""
