d2a5b89845a1110d38091ad4cd808ce2ed5db763
Or Bin <orbin50@gmail.com>
2022-01-28 13:09:09 +0200

    Replaced install_slack with computer_type, saving it into a settings file to avoid asking it each time. Also made CI run flows for both personal and work computers

--- tests/exec_setup.sh
+++ tests/exec_setup.sh
@@ -5,4 +5,10 @@ TESTS_DIR=$(dirname "${BASH_SOURCE}")
 
 source "${TESTS_DIR}/compute_env.sh"
 
-multipass exec "${INSTANCE_NAME}" -- bash -c "ANSIBLE_BECOME_ASK_PASS=0 /${REPO_NAME}/setup.sh --extra-vars \"@/${REPO_NAME}/tests/test_vars.yml\""
+computer_type_flags=()
+
+if [ -n "${COMPUTER_TYPE}" ]; then
+  computer_type_flags=("--extra-vars" "computer_type=${COMPUTER_TYPE}")
+fi
+
+multipass exec "${INSTANCE_NAME}" -- bash -c "ANSIBLE_BECOME_ASK_PASS=0 /${REPO_NAME}/setup.sh --extra-vars \"@/${REPO_NAME}/tests/test_vars.yml\" ${computer_type_flags[@]}"
