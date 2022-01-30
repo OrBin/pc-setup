00479c9090c70b7e8d197c6b6b906d6943075bca
Or Bin <orbin50@gmail.com>
2022-01-30 22:39:41 +0200

    Fixed bash misbehavior in exec_setup.sh

--- tests/exec_setup.sh
+++ tests/exec_setup.sh
@@ -11,4 +11,5 @@ if [ -n "${COMPUTER_TYPE}" ]; then
   computer_type_flags=("--extra-vars" "computer_type=${COMPUTER_TYPE}")
 fi
 
-multipass exec "${INSTANCE_NAME}" -- bash -c "ANSIBLE_BECOME_ASK_PASS=0 /${REPO_NAME}/setup.sh --extra-vars \"@/${REPO_NAME}/tests/test_vars.yml\" ${computer_type_flags[@]}"
+setup_command="ANSIBLE_BECOME_ASK_PASS=0 /${REPO_NAME}/setup.sh --extra-vars \"@/${REPO_NAME}/tests/test_vars.yml\" ${computer_type_flags[@]}"
+multipass exec "${INSTANCE_NAME}" -- bash -c "${setup_command}"
