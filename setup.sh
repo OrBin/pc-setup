8d24621c5866e9bf3ae84a1d11309b0e0f9258cd
Or Bin <orbin50@gmail.com>
2022-10-18 22:40:25 +0300

    Fixed setup.sh force-update

--- setup.sh
+++ setup.sh
@@ -11,13 +11,16 @@ install_pip() {
 sudo python3 -m pip > /dev/null 2>&1 || install_pip
 sudo python3 -m pip install --root-user-action=ignore -r requirements.txt
 
-galaxy_requirements_installed="${REPO_ROOT}/.local_state/ansible_galaxy_requirements_installed"
+galaxy_requirements_installed="${HOME}/.config/pc-setup/ansible_galaxy_requirements_installed"
 galaxy_requirements_spec="${REPO_ROOT}/requirements.yml"
 
 if [ ! -f $galaxy_requirements_installed ] || [ $(stat --format '%Y' $galaxy_requirements_installed) -lt $(stat --format '%Y' $galaxy_requirements_spec) ]; then
+  echo "Installing ${galaxy_requirements_spec} using ansible-galaxy"
   ansible-galaxy install -r $galaxy_requirements_spec --force
   mkdir -p $(dirname $galaxy_requirements_installed)
   touch $galaxy_requirements_installed
+else
+    echo "Requirements are already installed, skipping"
 fi
 
 time ANSIBLE_STDOUT_CALLBACK=debug ANSIBLE_BECOME_ASK_PASS=${ANSIBLE_BECOME_ASK_PASS:-1} ansible-playbook -i "${REPO_ROOT}/inventory.ini" "$@" "${REPO_ROOT}/main.yml"
