296dfcd11632ef4adc4b837bed6e2a1983d76519
Or Bin <orbin50@gmail.com>
2022-11-01 15:19:48 +0200

    Increasing resources for tests and CI

--- tests/test.sh
+++ tests/test.sh
@@ -14,6 +14,6 @@ fi
 "${TESTS_DIR}/validate_multipass.sh"
 
 multipass delete -p "${INSTANCE_NAME}" || true
-multipass launch --name "${INSTANCE_NAME}" --disk 15G "${cpu_flags[@]}" "${UBUNTU_VERSION}"
+multipass launch --name "${INSTANCE_NAME}" --disk 25G "${cpu_flags[@]}" "${UBUNTU_VERSION}"
 multipass mount "${REPO_ROOT}" "${INSTANCE_NAME}:/${REPO_NAME}"
 "${TESTS_DIR}/exec_setup.sh"
