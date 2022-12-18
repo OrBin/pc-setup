803db4b5c639ca2c539f1636119b005184824f47
Or Bin <orbin50@gmail.com>
2022-12-19 00:15:34 +0200

    Make interactive password input stuff happen only when not in CI

--- setup.sh
+++ setup.sh
@@ -18,9 +18,11 @@ install_pip() {
   wget -qO- https://bootstrap.pypa.io/get-pip.py | sudo python3 -
 }
 
-password=$(get_password)
-sudo -S -k true <<< "${password}"  # Calling sudo once with the password, to validate
-sudo -S true <<< "${password}"  # Calling sudo once again, to make it remember the permissions (which -k had reset before)
+if [[ ! -v CI ]]; then
+  password=$(get_password)
+  sudo -S -k true <<< "${password}"  # Calling sudo once with the password, to validate
+  sudo -S true <<< "${password}"  # Calling sudo once again, to make it remember the permissions (which -k had reset before)
+fi
 
 sudo python3 -m pip > /dev/null 2>&1 || (echo "pip is missing, installing" && install_pip)
 sudo python3 -m pip install --root-user-action=ignore -r "${REPO_ROOT}/requirements.txt"
