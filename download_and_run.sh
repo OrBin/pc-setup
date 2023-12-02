3eaf9fd63d4a827c0f507a39e84afea85c0f8c29
Or Bin <orbin50@gmail.com>
2023-12-02 11:53:27 +0200

    TEMP: update branch name

--- download_and_run.sh
+++ download_and_run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-wget --no-cache https://codeload.github.com/OrBin/pc-setup/tar.gz/refs/heads/gnome -O /tmp/pc-setup.tar.gz  # TODO revert
+wget --no-cache https://codeload.github.com/OrBin/pc-setup/tar.gz/refs/heads/gnome-work-temp -O /tmp/pc-setup.tar.gz  # TODO revert
 rm -rf /tmp/pc-setup/ && mkdir -p /tmp/pc-setup/
 tar -xvzf /tmp/pc-setup.tar.gz -C /tmp/pc-setup/ --strip-components=1
 rm /tmp/pc-setup.tar.gz
